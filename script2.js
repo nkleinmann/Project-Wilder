15ca366c476dc3d91ad9067445a91744664298da
nkleinmann <nekleinmann@gmail.com>
2020-07-31 07:36:39 -0400

    Updated comments and spelling of category in html

--- script2.js
+++ script2.js
@@ -1,7 +1,15 @@
 $(function (){
 
-    $(".btnCollection").on("click", function () {
+    $("#btnCollection").on("click", function () {
         console.log("button working");
+
+        // When collection button is clicked
+
+        // show everything that has been stored locally 
+
+
+
+
         // var settings = {
         //     "url": "https://api.nasa.gov/planetary/apod?api_key=vpUsbRtc2uBC7le2YXjPI6Nk1E5HtnsevXOvkGTe&date=" + searchDate,
         //     "method": "GET",
@@ -20,5 +28,9 @@ $(function (){
             
         //  });
     })
+
+    // Generate a remove button to delete relevant images 
+
+
 
 });
