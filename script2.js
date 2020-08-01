ebe87471f85849f63e0b740a9164e16a2985e29d
MacksM3 <64023042+macksm3@users.noreply.github.com>
2020-07-31 20:37:53 -0400

    fixed some js stuff

--- script2.js
+++ script2.js
@@ -1,60 +1,68 @@
 $(function (){
 
     // Photo array. This will have to change once save button works.
-    let photoArray = ["https://apod.nasa.gov/apod/image/2007/N6188_Cappelletti_960.jpg", "https://apod.nasa.gov/apod/image/2007/Mars2020launchKraus1024.jpg", 
-    "https://apod.nasa.gov/apod/image/2007/Kristine-Rose-Photography-20200616_001s1024.jpg", "https://apod.nasa.gov/apod/image/2007/ldn1251_jerahian1024.jpg", 
-    "https://apod.nasa.gov/apod/image/2007/msv1000crop.jpg"]
-    console.log(photoArray);
+    let photoArray = [
+        "https://apod.nasa.gov/apod/image/2007/Mars2020launchKraus1024.jpg", 
+        "https://apod.nasa.gov/apod/image/2007/Kristine-Rose-Photography-20200616_001s1024.jpg", 
+        "https://apod.nasa.gov/apod/image/2007/N6188_Cappelletti_960.jpg", 
+        "https://apod.nasa.gov/apod/image/2007/ldn1251_jerahian1024.jpg", 
+        "https://apod.nasa.gov/apod/image/2007/msv1000crop.jpg"
+    ]
+    // console.log(photoArray);
 
+    const imgDisplay = $("#imgCollection");
     let photoIndex = 0;
 
-    $("#btnCollection").on("click", function (event) {
-        // Stops page from refreshing when button is clicked
-        event.preventDefault();
-
+    $(imgDisplay).attr("src", "https://via.placeholder.com/400x300.jpg/000044/ffffff?text=Pictures+From+Space");
     
-        photoArray.forEach(function(photoURL) {
-            console.log(photoURL);
-        
-            let imageURL = photoURL;
-            $("#imgSearch").attr("src", imageURL);
-            //Need to call local storage
-          
-        }) ;
-
-        
-
-        
-        // show everything that has been stored locally 
-
-
-    })
-
-    
-    $("#btnNext").on("click", function(event) {
+    $("#btnCollectionNext").on("click", function(event) {
         event.preventDefault();
         imageURL = photoArray[photoIndex];
-        $("#imgSearch").attr("src", imageURL);
+        console.log(`image ${photoIndex} is ${imageURL}`);
+        $(imgDisplay).attr("src", imageURL);
+        
         photoIndex++;
-
         if (photoIndex >= photoArray.length) {
             photoIndex = 0;
         };
 
     })
 
-    $("#btnPrevious").on("click", function(event) {
+    $("#btnCollectionPrev").on("click", function(event) {
         event.preventDefault();
         imageURL = photoArray[photoIndex];
-        $("#imgSearch").attr("src", imageURL);
+        console.log(`image ${photoIndex} is ${imageURL}`);
+        $(imgDisplay).attr("src", imageURL);
+
         photoIndex--;
         if (photoIndex <= 0) {
-            photoIndex = photoArray.length;
+            photoIndex = photoArray.length - 1;
         };
     })
 
     // Generate a remove button to delete relevant images 
 
+    // $("#btnCollection").on("click", function (event) {
+    //     // Stops page from refreshing when button is clicked
+    //     event.preventDefault();
+
+    
+    //     // photoArray.forEach(function(photoURL) {
+    //     //     console.log(photoURL);
+        
+    //     //     let imageURL = photoURL;
+    //     //     $("#imgSearch").attr("src", imageURL);
+    //     //     //Need to call local storage
+          
+    //     // }) ;
+
+        
+
+        
+    //     // show everything that has been stored locally 
+
+
+    // })
 
 
 });
