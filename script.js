988c31e283d3bc84f0e55a19dfb9e98250151a65
SAR-SA <shawn.a.rather@gmail.com>
2020-08-01 10:18:49 -0400

    Last nights work

--- script.js
+++ script.js
@@ -8,7 +8,32 @@ $(function () {
     var searchDate = "2020-07-28";
     const apodImg = $("#imgSearch");
     const photoDetails = $("#textHere");
+    let marsQ = [];
 
+    $("#Btn1").on("click", function () {
+        var settings = {
+            "url": "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=999&camera=mast&api_key=DEMO_KEY",
+            "method": "GET",
+            "timeout": 0,
+          };
+          
+          $.ajax(settings).done(function (response) {
+            //console.log(response);
+          })
+            .then(function (response) {
+                let results = response.data;
+                console.log(results);
+                for (let i = 0; i < 10; i++) {
+                    console.log(response.photos.img_src [i]);
+                    marsQ [i] = response.photos.img_src [i];
+                }
+                // const imgURL = response.img_src;
+                // apodImg.attr("src", imgURL);
+                // // const apodDiv = $("<div>");
+                // const photoText = response.camera;
+                // photoDetails.text(photoText);
+            });
+    });
 
     $("#Btn2").on("click", function () {
         var settings = {
@@ -20,17 +45,20 @@ $(function () {
         $.ajax(settings).done(function (response) {
             console.log(response);
         })
-        .then(function(response) {
-            let results = response.data;
-            console.log(results);
-            const imgURL = response.url;
-            apodImg.attr("src", imgURL);
-            // const apodDiv = $("<div>");
-            const photoText = response.explanation;
-            photoDetails.text(photoText);
-         });
-    })
-    
+            .then(function (response) {
+                let results = response.data;
+                console.log(results);
+                const imgURL = response.url;
+                apodImg.attr("src", imgURL);
+                // const apodDiv = $("<div>");
+                const photoText = response.explanation;
+                photoDetails.text(photoText);
+            });
+    });
+
+
+
+
 
     // Dynamically add elements to page to display image 
 
